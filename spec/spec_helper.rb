c7826bbc9decf8d6e7a83c1e65731d494fd528ea
Alex Smith <alex.geoffrey.smith@gmail.com>
2014-05-09 21:55:59 +1000

    generate rspec configuration

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+
+  config.order = "random"
+end

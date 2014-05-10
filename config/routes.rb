d1acd300c1bb8606b3c2cbfbd4866ea66c91d83c
Haizyfox <bradbarrow@gmail.com>
2014-05-10 15:12:43 +1000

    Memberships page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "/buy",   to: "memberships#new", as: :new_membership
   get "/about", to: "static_pages#about", as: :about
+  get "/plans", to: "memberships#index", as: :memberships
   resource :memberships,
     only: [:create]
 end

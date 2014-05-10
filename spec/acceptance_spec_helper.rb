36696fc8e7a51486126bb20f710693f95bb32fe8
Alex Smith <alex.geoffrey.smith@gmail.com>
2014-05-10 13:47:41 +1000

    use poltergeist as capybara driver

--- spec/acceptance_spec_helper.rb
+++ spec/acceptance_spec_helper.rb
@@ -1,3 +1,6 @@
 require 'spec_helper'
 
-require 'capybara/rspec'
+require 'capybara/rails'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+Capybara.default_driver    = :poltergeist

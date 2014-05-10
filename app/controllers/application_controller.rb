6d6d1863703e7374fa2d931fc439c43661c893bb
Leni Mayo <leni@moniker.net>
2014-05-10 18:39:52 +1000

    ApplicationController has a logged_in? helper stub

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def logged_in?
+    # @ags TODO - replace this stub
+    true
+  end
+  helper_method :logged_in?
+
 end

7717d855c2fd230ca285a5a0f8a5b186cf3564a7
Alex Smith <alex.geoffrey.smith@gmail.com>
2014-05-10 17:37:40 +1000

    fix conflicting memberships route name

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get "/buy",   to: "memberships#new", as: :new_membership
+  post "/memberships", to: "memberships#create", as: :create_membership
   get "/plans", to: "memberships#index", as: :memberships
 
   get "/login", to: "authentication#login", as: :login
@@ -11,6 +12,5 @@ Rails.application.routes.draw do
   get "/contact", to: "static_pages#contact", as: :contact
   get "/terms", to: "static_pages#terms_and_conditions", as: :terms_and_conditions
   get "/privacy", to: "static_pages#privacy", as: :privacy
-  resource :memberships,
-    only: [:create]
+
 end

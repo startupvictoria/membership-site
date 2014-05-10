68a955bbc8ef7690ee70de6edcf75b685976a604
Haizyfox <bradbarrow@gmail.com>
2014-05-10 15:40:01 +1000

    Forgot password html

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/plans", to: "memberships#index", as: :memberships
 
   get "/login", to: "authentication#login", as: :login
+  get "/forgot_password", to: "authentication#forgot_password", as: :forgot_password
 
   get "/about", to: "static_pages#about", as: :about
   get "/contact", to: "static_pages#contact", as: :contact

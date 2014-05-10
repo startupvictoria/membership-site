8d31acd7a5da962acead94d5167fd19b982a48d3
Haizyfox <bradbarrow@gmail.com>
2014-05-10 14:37:19 +1000

    Add an about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  get "/buy", to: "memberships#new", as: :new_membership
+  get "/buy",   to: "memberships#new", as: :new_membership
+  get "/about", to: "static_pages#about", as: :about
   resource :memberships,
     only: [:create]
 end

adc8f0852fdbe4ab1d62eef3936bae2c808a48d1
Haizyfox <bradbarrow@gmail.com>
2014-05-10 15:22:36 +1000

    Add the contact us page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def about
   end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get "/buy",   to: "memberships#new", as: :new_membership
-  get "/about", to: "static_pages#about", as: :about
   get "/plans", to: "memberships#index", as: :memberships
+
+  get "/about", to: "static_pages#about", as: :about
+  get "/contact", to: "static_pages#contact", as: :contact
   resource :memberships,
     only: [:create]
 end

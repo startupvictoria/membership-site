54db33a2d101d7dd5a560668d72f9228133b355a
Leni Mayo <leni@moniker.net>
2014-05-12 09:04:48 +1000

    /admin/users etc

---
+++ app/admin/user.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register User do
+
+  permit_params :email, :full_name, :admin
+
+  filter :email
+  filter :full_name
+  filter :created_at
+
+  index do
+    column :email
+    column :full_name
+    column :admin
+    column :created_at
+
+    default_actions
+  end
+
+end

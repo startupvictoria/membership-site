3b04d0ea758bac4bad5a9f63ce9541652bf58759
Leni Mayo <leni@moniker.net>
2014-05-13 02:16:15 +1000

    ContactMailer copes with dodgy input

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,8 +4,18 @@ class ContactMailer < ActionMailer::Base
 
     mail(
       to:      "contact@startupvictoria.com.au",
-      from:    "\"#{attributes[:full_name]}\" <#{attributes[:email]}>",
-      subject: "contact: #{attributes[:subject]}"
+      from:    "\"#{white_in_from(:full_name)}\" <#{white_in_from(:email)}>",
+      subject: "contact: #{white_in_subject(:subject)}"
     )
+  end
+
+  private
+
+  def white_in_from(key)
+    @attributes[key].gsub(/[\r\n<>\"]/, '').slice(0..253)
+  end
+
+  def white_in_subject(key)
+    @attributes[key].gsub(/[\r\n]/, '').slice(0..998)
   end
 end

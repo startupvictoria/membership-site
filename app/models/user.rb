1510944663fbdd0e49f3c6bbd538804eb1a4aa31
Leni Mayo <leni@moniker.net>
2014-05-12 23:04:52 +1000

    user faction is an enum #2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
   has_one :membership,
     through: :customer
 
-  enum faction: { unknown: 0, non_founder: 1, aspiring_founder: 2, practising_founder: 3 }
+  enum faction: { unassigned: 0, civilian: 1, aspirant: 2, practitioner: 3 }
 end

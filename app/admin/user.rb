a7734b63e814973a9b6980ca2959b946ff2340b9
Leni Mayo <leni@moniker.net>
2014-05-12 14:25:00 +1000

    ActiveAdmin.register User knows about User.faction

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,18 +1,44 @@
 ActiveAdmin.register User do
 
-  permit_params :email, :full_name, :admin
+  permit_params :email, :full_name, :faction, :admin
 
   filter :email
   filter :full_name
+  filter :faction
   filter :created_at
 
   index do
     column :email
     column :full_name
+    column :faction
     column :admin
     column :created_at
 
     default_actions
+  end
+
+   show do |user|
+     attributes_table do
+       row :email
+       row :full_name
+       row :faction
+       row :admin
+       row :created_at
+     end
+     active_admin_comments
+   end
+
+  form do |f|
+    f.inputs "UserDetails" do
+      f.input :email
+      f.input :full_name
+      f.input :faction,
+        as: :radio,
+        collection: User.factions.keys,
+        selected: f.object.faction
+      f.input :admin
+    end
+    f.actions
   end
 
 end

a79d035b350102c9331df0da89007b6e3fab1bfc
Leni Mayo <leni@moniker.net>
2014-05-13 13:54:17 +1000

    /admin/dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,51 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "New Members" do
+          para "Last 7 days: #{User.with_membership.created_last_n_days(7).count}"
+          para "Last 30 days: #{User.with_membership.created_last_n_days(30).count}"
+        end
+
+        panel "New members last 7 days" do
+          table_for User.with_membership.last(5).reverse do
+            column "Name" do |u|
+              link_to(u.full_name, admin_member_path(u.id))
+            end
+            column "email", :email
+            column "Faction", :faction
+            column "Created_at", :created_at
+          end
+        end
+      end
+
+      column do
+        panel "Membership Composition" do
+          table_for admin_membership_subtotals do
+            column "Faction", :faction
+            column "count", :c
+          end
+        end
       end
     end
+  end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  controller do
+    def admin_membership_subtotals
+      total = { faction: "TOTAL", c: User.with_membership.count }
+      ret = User.
+        with_membership.
+        select("faction, count(*) AS c").
+        group(:faction).
+        map do |o|
+          { faction: o.faction, c: o.c }
+        end.sort do |a, b|
+          b[:c] <=> a[:c]
+        end
+        ret.push total
+        ret
+    end
+    helper_method :admin_membership_subtotals
+  end
 end

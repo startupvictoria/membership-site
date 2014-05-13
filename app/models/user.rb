98302110bafc2a22772699ea1577ec1b36ad41e3
Leni Mayo <leni@moniker.net>
2014-05-13 13:53:57 +1000

    User scope :created_last_n_days

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,6 @@ class User < ActiveRecord::Base
   enum faction: { unassigned: 0, civilian: 1, aspirant: 2, practitioner: 3 }
 
   scope :with_membership, -> { joins(:membership) }
+
+  scope :created_last_n_days, ->(n) { where("users.created_at >= ?", n.days.ago) }
 end

2e926516f1d5e4a3e97e839a2d5f5e3ae61a8fd9
Leni Mayo <leni@moniker.net>
2014-05-21 14:08:18 +1000

    redirect www to non-www

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  constraints subdomain: 'www' do
+    get ':any', to: redirect(subdomain: nil, path: '/%{any}'), any: /.*/
+  end
+
   ActiveAdmin.routes(self)
 
   root to: "home#index"

ed4571524909f473da320b98312319a6d8118260
Leni Mayo <leni@moniker.net>
2014-05-26 22:33:01 +1000

    use font-url(), precompile bootstrap glyphicon font
    url('../assets/') works in dev, breaks in production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,10 @@
 StartupVictoria.config.assets.precompile += [
   "on-form-error.js",
-  "membership-new.js"
+  "membership-new.js",
+
+  # bootstrap glyphicon font
+  "glyphicons-halflings-regular.eot",
+  "glyphicons-halflings-regular.woff",
+  "glyphicons-halflings-regular.ttf",
+  "glyphicons-halflings-regular.svg",
 ]

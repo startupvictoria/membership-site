be902156e0078bae87ffe90c7afd471b230ef8de
Leni Mayo <leni@moniker.net>
2014-05-28 05:57:47 +1000

    /events query params ?venue and ?organizer

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,13 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Event.upcoming.all.map { |event| event.decorate }
+    @events = \
+      Event
+        .with_venue(params[:venue])
+        .with_organizer(params[:organizer])
+        .upcoming
+        .all
+        .map { |event| event.decorate }
   end
 
 end

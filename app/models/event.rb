55fc817c8f10f656e5e1a764aa98880c13db7b7f
Leni Mayo <leni@moniker.net>
2014-05-28 05:56:56 +1000

    Event scopes: with_venue and with_organizer

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,15 @@ class Event < ActiveRecord::Base
   belongs_to :event_venue
   belongs_to :event_organizer
 
-  scope :upcoming, -> { where("starts_at > ?", Time.now).order("events.starts_at ASC") }
+  scope :upcoming, -> do
+    where("starts_at > ?", Time.now).order("events.starts_at ASC")
+  end
+
+  scope :with_venue, ->(arg) do
+    joins(:event_venue).where("event_venues.code IN (?)", arg) unless arg.nil?
+  end
+
+  scope :with_organizer, ->(arg) do
+    joins(:event_organizer).where("event_organizers.code IN (?)", arg) unless arg.nil?
+  end
 end

d5a33ebd594ce4769d820af985189a10eef83621
Leni Mayo <leni@moniker.net>
2014-05-28 14:14:00 +1000

    EventOrganizer before_validation code.downcase

--- app/models/event_organizer.rb
+++ app/models/event_organizer.rb
@@ -1,4 +1,8 @@
 class EventOrganizer < ActiveRecord::Base
+  before_validation do |eo|
+    eo.code = eo.code.downcase
+  end
+
   def self.from_param(code)
     EventOrganizer.find_by_code code.to_s
   end

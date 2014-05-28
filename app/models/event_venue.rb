767e84346b12fcef498939a7e3488ba4ab329d37
Leni Mayo <leni@moniker.net>
2014-05-28 14:14:20 +1000

    EventVenue before_validation code.downcase

--- app/models/event_venue.rb
+++ app/models/event_venue.rb
@@ -1,4 +1,8 @@
 class EventVenue < ActiveRecord::Base
+  before_validation do |ev|
+    ev.code = ev.code.downcase
+  end
+
   def self.from_param(code)
     EventVenue.find_by_code code.to_s
   end

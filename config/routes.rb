65945c419c2d148ac9284da90bdc9c52b719aadf
Leni Mayo <leni@moniker.net>
2014-06-19 21:15:44 +1000

    routes organised alphabetically

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,15 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
-  get "/login", to: "authentication#login",    as: :login
+  get  "/about/us", to: "about#us", as: :about_us
   post "/authenticate", to: "authentication#create", as: :authenticate
-  get "/logout", to: "authentication#destroy",    as: :logout
+  get  "/contact-us", to: "contacts#new", as: :new_contact
+  get  "/login", to: "authentication#login", as: :login
+  get  "/logout", to: "authentication#destroy", as: :logout
+  get  "/privacy", to: "static_pages#privacy", as: :privacy
+  get  "/terms", to: "static_pages#terms_and_conditions", as: :terms_and_conditions
 
-  get "/about/us", to: "about#us", as: :about_us
-  get "/terms", to: "static_pages#terms_and_conditions", as: :terms_and_conditions
-  get "/privacy", to: "static_pages#privacy", as: :privacy
-
-  resources :memberships,
-    only: [:index, :create, :new]
+  resource :contacts, only: [:create]
 
   resources :coupons,
     only: [:index, :show]
@@ -26,10 +25,11 @@ Rails.application.routes.draw do
   resources :events,
     only: [:index]
 
-  get "/contact-us", to: "contacts#new", as: :new_contact
-  resource :contacts, only: [:create]
+  resources :memberships,
+    only: [:index, :create, :new]
 
   resources :password_resets,
     only: [:index, :show, :create, :update],
     path: "forgot-password"
+
 end

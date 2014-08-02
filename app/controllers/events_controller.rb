638181c5f787469ce960adecb7e0d7a7a1e09284
LarsOZ <lars@readcloud.com>
2014-08-02 11:25:59 +0200

    test change to events controller
    trying to not show old events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,9 @@ class EventsController < ApplicationController
   def index
     @events = \
       Event
+        .with_venue(params[:venue])
+        .with_organizer(params[:organizer])
+        .upcoming
         .all
         .map { |event| event.decorate }
   end

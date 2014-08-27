69833f472131500f7146ebc2875ed2e58361d34e
Leni Mayo <leni@moniker.net>
2014-08-27 13:51:01 +1000

    refactor CreatesMembership to expose create_customer_and_membership

--- app/services/creates_membership.rb
+++ app/services/creates_membership.rb
@@ -6,19 +6,21 @@ class CreatesMembership
   attr_accessor :card_token
 
   def call
-    stripe_customer = Stripe::Customer.create(
+    stripe_customer = Stripe::Customer.create \
       email: user.email,
       description: "User ##{user.id}",
       card: card_token
-    )
 
     stripe_customer.subscriptions.create(plan: plan.id)
 
-    customer = Customer.create!(
+    create_customer_and_membership stripe_customer.id, stripe_customer.default_card
+  end
+
+  def create_customer_and_membership(stripe_id = nil, stripe_card_id = nil)
+    customer = Customer.create! \
       user_id: user.id,
-      stripe_id: stripe_customer.id,
-      stripe_card_id: stripe_customer.default_card
-    )
+      stripe_id: stripe_id,
+      stripe_card_id: stripe_card_id
 
     Membership.create!(customer_id: customer.id, plan_id: plan.id)
 

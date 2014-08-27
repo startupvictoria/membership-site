3f88448bf70758b5b43127b94696b01fdd0fdf04
Leni Mayo <leni@moniker.net>
2014-08-28 04:36:41 +1000

    MembershipPlan.all

--- app/models/membership_plan.rb
+++ app/models/membership_plan.rb
@@ -2,7 +2,12 @@ class MembershipPlan < Struct.new(:id, :price_cents)
   def self.premium
     new("premium", 9900)
   end
+
   def self.free
     new("free", 0)
+  end
+
+  def self.all
+    [ "premium", "free" ]
   end
 end

31ff22acbcc8ccb782489555114e77909850c361
Leni Mayo <leni@moniker.net>
2014-08-28 04:37:04 +1000

    Membership validates plan_id

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,10 @@
 class Membership < ActiveRecord::Base
   audited
 
+  validates :plan_id,
+    inclusion: {
+      in: MembershipPlan.all,
+      message: "%{value} is not a valid MembershipPlan.id" }
+
   belongs_to :customer
 end

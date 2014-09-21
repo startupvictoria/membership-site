4e29e3700994da487123465742a2ec424f57e35e
Leni Mayo <leni@moniker.net>
2014-09-21 21:51:57 +1000

    FactoryGirl factories for EventOrganizer, EventVenue and Event

---
+++ spec/factories/event.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :event do
+    # association :event_venue, factory: :event_venue, strategy: :build, code: "ybf"
+    event_venue
+    event_organizer
+    title "The 21st Century CSIRO"
+    starts_at Time.now
+  end
+end
---
+++ spec/factories/event_organizer.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :event_organizer do
+    sequence(:code) { |n| "code#{n}" }
+    sequence(:name) { |n| "name#{n}" }
+  end
+end

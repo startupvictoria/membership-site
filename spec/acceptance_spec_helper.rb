7c851d4de9194c970c2842dea419030d458c11b8
Jay Lynch <hi@jaylyn.ch>
2018-07-07 15:47:41 +1000

    Fix Stripe loading in Travis

--- spec/acceptance_spec_helper.rb
+++ spec/acceptance_spec_helper.rb
@@ -5,5 +5,12 @@ require 'capybara/rails'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
 
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {
+    js_errors: true,
+    phantomjs_options: ['--ignore-ssl-errors=yes', '--ssl-protocol=any'],
+    timeout: 240
+  })
+end
 Capybara.default_driver = :poltergeist
 Capybara.default_max_wait_time = 10

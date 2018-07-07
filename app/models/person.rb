850abfd3a90d8e9caa35ac0e24b027125e08e47f
Jay Lynch <hi@jaylyn.ch>
2018-07-07 16:37:00 +1000

    Update people and allow retina images

--- app/models/person.rb
+++ app/models/person.rb
@@ -22,43 +22,59 @@ class Person < Struct.new(:name, :email, :bio, :image_filename, :url, :role)
   def self.all
     [
       [
+        "Peter Cameron",
+        "",
+        "Peter is an angel investor, venture capitalist and chairman of Startup Victoria.",
+        "Peter-Cameron.jpg",
+        "https://www.linkedin.com/in/petecameron/",
+        :director
+      ],
+      [
         "Leni Mayo",
         "",
-        "Leni is a software engineer, angel investor and chairman of Startup Victoria.",
+        "Leni is a software engineer, angel investor and co-founder of Startup Victoria.",
         "Leni-Mayo.png",
         "http://moniker.net/leni/bio/",
         :director
       ],
       [
-        "Michelle Bourke",
-        "",
-        "Michelle is the director of Artlivemedia and co-director of The Talent Door.",
-        "Michelle-Bourke.png",
-        "https://au.linkedin.com/in/michellebourke/",
-        :director
-      ],
-      [
         "Scott Handsaker",
         "",
-        "Scott is the CEO of Attendly and Eventarc and a mentor at AngelCube.",
-        "Scott-Handsaker.png",
+        "Scott is CEO of Attendly and CyRise, a mentor at Startmate and co-founder of Startup Victoria.",
+        "Scott-Handsaker.jpg",
         "https://au.linkedin.com/in/scotthandsaker/",
         :director
       ],
       [
-        "Sarah Hamilton",
+        "Jessica Kerr",
         "",
-        "Sarah is co-founder and CEO of bellabox.",
-        "Sarah-Hamilton.jpg",
-        "https://au.linkedin.com/in/sarahhamilton4",
+        "Jessica is Director of Sinclair + May, a female-led boutique commercial law firm.",
+        "Jessica-Kerr.jpg",
+        "https://www.linkedin.com/in/jessicaskerr/",
         :director
       ],
       [
-        "Georgia Beattie",
-        "georgia.beattie@startupvictoria.com.au",
+        "Morgan Ranieri",
+        "",
+        "Morgan is a co-founder and CEO at YourGrocer.",
+        "Morgan-Ranieri.jpg",
+        "https://www.linkedin.com/in/morganranieri/",
+        :director
+      ],
+      [
+        "Alan Dee",
+        "",
+        "Alan is co-founder and CEO at Moonshot Works and a mentor at La Trobe Accelerator.",
+        "Alan-Dee.jpg",
+        "https://www.linkedin.com/in/alandee/",
+        :director
+      ],
+      [
+        "Judy Anderson",
+        "judy.anderson@startupvictoria.com.au",
         "Chief Executive Officer",
-        "georgia_beattie.png",
-        "https://au.linkedin.com/in/gbeattie/",
+        "Judy-Anderson.jpg",
+        "https://www.linkedin.com/in/fasttrackjudy/",
         :staff
       ],
       [
@@ -77,6 +93,14 @@ class Person < Struct.new(:name, :email, :bio, :image_filename, :url, :role)
         "https://www.linkedin.com/in/meiyuanchan/",
         :staff
       ],
+      [
+        "Siddharth Vadasseri",
+        "sid@startupvictoria.com.au",
+        "Community and Events Co-ordinator",
+        "Siddharth-Vadasseri.jpg",
+        "https://www.linkedin.com/in/siddharth-vadasseri-131b195a/",
+        :staff
+      ]
     ].map do |a|
       new *a
     end
